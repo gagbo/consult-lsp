c4c0426b58946578ac1806a60258d2b275d5b524
Gerry Agbobada <gerry@gagbo.net>
2021-04-25 04:55:21 +0200

    chore(melpa): add more information in commentary
    This information is used to generate description on MELPA

--- consult-lsp.el
+++ consult-lsp.el
@@ -30,9 +30,30 @@
 
 ;;; Commentary:
 ;; Provides LSP-mode related commands for consult
-
-;; TODO: Use a custom format for the propertized candidates
-;; TODO: Check the properties in LSP to see how sources should be used
+;;
+;; The commands are autoloaded so you don't need to require anything to make them
+;; available. Just use M-x and go!
+;;
+;;;; Diagnostics
+;; M-x consult-lsp-diagnostics provides a view of all diagnostics in the current
+;; workspace (or all workspaces if passed a prefix argument).
+;;
+;; You can use prefixes to filter diagnostics per severity, and
+;; previewing/selecting a candidate will go to it directly.
+;;
+;;;; Symbols
+;; M-x consult-lsp-symbols provides a selection/narrowing command to search
+;; and go to any arbitrary symbol in the workspace (or all workspaces if
+;; passed a prefix argument).
+;;
+;; You can use prefixes as well to filter candidates per type, and
+;; previewing/selecting a candidate will go to it.
+;;
+;;;; Contributions
+;; Possible contributions for ameliorations include:
+;; - using a custom format for the propertized candidates
+;; - checking the properties in LSP to see how diagnostic-sources should be used
+;; - checking the properties in LSP to see how symbol-sources should be used
 ;;; Code:
 
 (require 'consult)
